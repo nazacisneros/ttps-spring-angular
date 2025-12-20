a1aaac7758e1915a0e8cbfb9b481dd691e99781f
Nazareno Cisneros <nazarenocisneros@hotmail.com>
2025-12-20 10:14:29 -0300

    [FIX]: se eliminó un import sin utilizar.

--- src/main/java/ttps/spring/controller/AuthController.java
+++ src/main/java/ttps/spring/controller/AuthController.java
@@ -3,7 +3,6 @@ package ttps.spring.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
